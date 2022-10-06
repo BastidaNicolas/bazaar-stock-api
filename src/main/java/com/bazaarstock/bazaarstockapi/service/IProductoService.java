48cc1e2f73556398df593cd4710aaa6bd9a13390
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2022-10-06 15:27:53 -0300

    Implemented the required features.

--- src/main/java/com/bazaarstock/bazaarstockapi/service/IProductoService.java
+++ src/main/java/com/bazaarstock/bazaarstockapi/service/IProductoService.java
@@ -1,4 +1,23 @@
 package com.bazaarstock.bazaarstockapi.service;
 
+import com.bazaarstock.bazaarstockapi.model.Producto;
+
+import java.util.List;
+
 public interface IProductoService {
+    List<Producto> getAll();
+
+    void delete(Long id);
+
+    Producto getProducto(Long id);
+
+    void create(Producto producto);
+
+    Producto getProductoMax();
+
+    Producto getProductoMin();
+
+    List<Producto> getProductoMinMax();
+
+    List<Producto> getProductoMaxMin();
 }
--- src/main/java/com/bazaarstock/bazaarstockapi/service/ProductoService.java
+++ src/main/java/com/bazaarstock/bazaarstockapi/service/ProductoService.java
@@ -1,8 +1,11 @@
 package com.bazaarstock.bazaarstockapi.service;
 
+import com.bazaarstock.bazaarstockapi.model.Producto;
 import com.bazaarstock.bazaarstockapi.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductoService implements IProductoService {
@@ -10,4 +13,43 @@ public class ProductoService implements IProductoService {
     @Autowired
     private ProductoRepository productoRepository;
 
+    @Override
+    public List<Producto> getAll() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        productoRepository.deleteById(id);
+    }
+
+    @Override
+    public Producto getProducto(Long id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void create(Producto producto) {
+        productoRepository.save(producto);
+    }
+
+    @Override
+    public Producto getProductoMax() {
+        return productoRepository.findByMaxPrice();
+    }
+
+    @Override
+    public Producto getProductoMin() {
+        return productoRepository.findByMinPrice();
+    }
+
+    @Override
+    public List<Producto> getProductoMinMax() {
+        return productoRepository.findAllByOrderByPriceAsc();
+    }
+
+    @Override
+    public List<Producto> getProductoMaxMin() {
+        return productoRepository.findAllByOrderByPriceDesc();
+    }
 }
