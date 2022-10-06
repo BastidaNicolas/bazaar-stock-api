a85aa989e2a201a7bb6f81242e4053c1f2eae8a7
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2022-10-06 09:54:23 -0300

    Initial commit

---
+++ src/main/java/com/bazaarstock/bazaarstockapi/BazaarStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.bazaarstock.bazaarstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaarStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaarStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bazaarstock/bazaarstockapi/BazaarStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazaarstock.bazaarstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaarStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
